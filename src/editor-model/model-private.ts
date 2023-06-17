a9814b97636d7a4a00bc9587da792c52c3e3dca7
manstie <manstie@hotmail.com>
2023-06-17 20:09:41 +0800

    kedyou: confine cursor to inside aligned environment

--- src/editor-model/model-private.ts
+++ src/editor-model/model-private.ts
@@ -31,8 +31,9 @@ import {
   isSelection,
   range,
 } from './selection-utils';
-import type { ArrayAtom } from '../atoms/array';
+import { ArrayAtom } from '../atoms/array';
 import { LatexAtom } from '../atoms/latex';
+import { isAlignEnvironment } from '../latex-commands/environment-types';
 import { makeProxy } from 'virtual-keyboard/mathfield-proxy';
 import '../virtual-keyboard/global';
 import type { ModelState, GetAtomOptions, AnnounceVerb } from './types';
@@ -268,6 +269,24 @@ export class _Model implements Model {
 
   get lastOffset(): Offset {
     return this.atoms.length - 1;
+  }
+
+  get lastOffsetOfAligned(): Offset {
+    let offset = this.lastOffset;
+    const atom = this.atoms[offset];
+    if (atom instanceof ArrayAtom && isAlignEnvironment(atom.environmentName))
+      offset--;
+
+    return offset;
+  }
+
+  get firstOffsetOfAligned(): Offset {
+    let offset = 0;
+    const atom = this.atoms[this.lastOffset];
+    if (atom instanceof ArrayAtom && isAlignEnvironment(atom.environmentName))
+      offset++;
+
+    return offset;
   }
 
   at(index: number): Atom {
@@ -708,8 +727,10 @@ export class _Model implements Model {
   }
 
   normalizeOffset(value: Offset): Offset {
-    if (value > 0) value = Math.min(value, this.lastOffset);
-    else if (value < 0) value = this.lastOffset + value + 1;
+    if (value > this.firstOffsetOfAligned)
+      value = Math.min(value, this.lastOffsetOfAligned);
+    else if (value < this.firstOffsetOfAligned)
+      value = this.firstOffsetOfAligned;
 
     return value;
   }
